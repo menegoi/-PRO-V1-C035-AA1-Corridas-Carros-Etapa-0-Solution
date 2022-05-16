c8d312ac3ef0146dd864465a21f3acc7bbb561d6
menegoi <liviamenegoi@gmail.com>
2022-05-16 18:44:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# -PRO-V1-C035-AA1-Corridas-Carros-Etapa-0-Solution
---
+++ sketch.js
@@ -0,0 +1,17 @@
+var gameState = 0;
+var playerCount = 0;
+
+var database;
+
+function setup(){
+  createCanvas(500,500);
+
+  //Banco de dados
+  database = firebase.database();
+
+}
+
+function draw(){
+  background(0);
+
+}
